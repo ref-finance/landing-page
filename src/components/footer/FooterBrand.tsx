2c9e076883eecab6794bc296592430c1bcb0080b
Max <maxwupro@outlook.com>
2022-02-10 16:07:10 +0800

    update footer logo position

--- src/components/footer/FooterBrand.tsx
+++ src/components/footer/FooterBrand.tsx
@@ -5,7 +5,7 @@ import { Logo } from '../pic';
 const FooterBrand = () => {
   return (
     <div className="s:col-span-3 s:mb-12 s:w-full">
-      <div className="flex items-center justify-center">
+      <div className="flex items-center s:justify-center">
         <Logo />
       </div>
       <div className="FooterBrand_content mx-auto mt-6 font-poppins text-sm text-left text-white opacity-50 s:w-80">
