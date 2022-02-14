0a952eac56585f9e1aa63bb8d4d77197e5820523
Max <maxwupro@outlook.com>
2022-02-14 18:37:41 +0800

    modfiy card boder UI

--- src/pages/home/Intro/DataCards.tsx
+++ src/pages/home/Intro/DataCards.tsx
@@ -89,10 +89,10 @@ const GradientBackground = () => {
 
 const DataCard: FC<DataCardProps> = ({ title, value, icon, description }) => {
   return (
-    <div className="dataCard relative font-bold italic text-white flex flex-col justify-center items-center text-center mx-2 s:my-2 rounded-2xl">
-      <div className="absolute">
+    <div className="dataCard relative font-bold italic text-white flex flex-col justify-center items-center text-center mx-2 s:my-2 rounded-2xl border border-mobile">
+      {/* <div className="absolute">
         <GradientBackground />
-      </div>
+      </div> */}
 
       <div
         style={{
