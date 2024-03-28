b3aeb9d86c9d15de703a8e46b65e09a428edf268
lq0-github <1441665200@qq.com>
2024-03-28 17:29:14 +0800

    Fix team spacing

--- src/pages/home/v3/Team/index.tsx
+++ src/pages/home/v3/Team/index.tsx
@@ -15,7 +15,7 @@ const Team = () => {
 
   return (
     <div className="relative">
-      <div className="relative z-10 text-center mx-auto mt-64 md:mt-24  sm:w-full md:w-full sm:pl-0">
+      <div className="relative z-10 text-center mx-auto mt-64 sm:mt-36 md:mt-24  sm:w-full md:w-full sm:pl-0">
         <div className="absolute -top-10 inset-1/3 sm:relative sm:flex sm:flex-col sm:justify-center sm:items-center sm:m-auto sm:top-0 sm:left-0 sm:right-0 sm:bottom-0">
           <div className="flex justify-center text-white gotham_font_bold  text-2xl">REF TEAM</div>
           <div
