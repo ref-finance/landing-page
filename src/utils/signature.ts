b89f5345357b916b22049240cc6dfbbb066f71d1
nature <nature@ref.finance>
2024-08-20 17:21:35 +0800

    remove test key

--- src/utils/signature.ts
+++ src/utils/signature.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto-browserify';
 
-const key = process.env.PARCEL_PUBLIC_CRYPTO_KEY || '8309c61008a5f5ba6c51bbf977781c55';
+const key = process.env.PARCEL_PUBLIC_CRYPTO_KEY
 
 export const getSignature = (plaintext: string) => {
   if (!key) return;
